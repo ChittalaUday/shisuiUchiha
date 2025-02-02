5d2a292d0a24774dd53f4dbe66746de977dbb29d
ProdigyHRM <167640342+ProdigyHRM@users.noreply.github.com>
2025-02-02 17:13:40 +0530

    many-updates

--- src/main.jsx
+++ src/main.jsx
@@ -1,12 +1,9 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import './index.css'
+import './index.css';
 import NavBar from './components/NavBar/NavBar';
 import Footer from './components/Footer/Footer';
-import HomePage from './pages/homepage.jsx'
-import AboutUs from './pages/AboutUs.jsx'
-import ClientPortal from './components/ClientPortal/ClientPortal.jsx'
-
+import HomePage from './pages/homepage.jsx';
 createRoot(document.getElementById('root')).render(
   <StrictMode>
     <NavBar />
